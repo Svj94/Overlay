d40e9bb07cc4c2e2f184a700e80cd5fd447d2895
Svj94 <shreyasvj94@gmail.com>
2017-10-23 19:28:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -136,7 +136,7 @@ var filesToCache = [
 	'/Overlay/scripts/lib/u-ribbon.html',
 	'/Overlay/scripts/lib/webcomponents.js',
 	'/Overlay/scripts/utility/pace.js',
-	'/Overlay/webcis/cis.csv',
+	'/Overlay/webcis/Cis.csv',
 	'/Overlay/webcis/BinaryMappings.csv',
 	'/Overlay/webcis/EUMainDashboardCISController.xml'
 ];
