71a6ee98dbf2fb531db176d563ae72ed8b24ace0
Svj94 <shreyasvj94@gmail.com>
2017-10-23 02:45:58 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+requirejs(["utility/pace"], function(pace) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "utility/pace".
+
+	
+		$(document).ready(function(){
+			$.ajax({
+					url: "http://localhost:8080/overlay.svc/Handshake",
+					type: 'GET',
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					async: false,
+					success: function (data) {
+						console.log(data.ClientSessionToken);
+					    var token = encodeURIComponent(data.ClientSessionToken);
+					    window.location = "login.html?token=" + token;
+					},
+					error: function (data) {
+						console.log("Overlay Server not available!");
+						
+					}
+				});				
+		});	
+		
+});
