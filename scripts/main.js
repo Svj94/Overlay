5cc01a2964a3bd0bd44f4bdd27b1b8839d905e6b
Svj94 <shreyasvj94@gmail.com>
2017-10-25 16:40:10 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,22 @@ requirejs(["utility/pace"], function(pace) {
         console.log('Notification permission status:', status);
         });	
 	
+	function displayNotification() {
+	  if (Notification.permission == 'granted') {
+	    navigator.serviceWorker.getRegistration().then(function(reg) {
+	     	var options = {
+		body: 'Here is a notification body!',
+		icon: 'images/144_Copy.png',
+		vibrate: [100, 50, 100],
+		data: {
+		  dateOfArrival: Date.now(),
+		  primaryKey: 1
+		}
+	      };
+	      reg.showNotification('Hello world!', options);
+	    });
+	  }
+	}
 	
 	
 	
