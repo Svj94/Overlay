b7c3c5a162edc4197aff2d36db916804bf3b8a97
Svj94 <shreyasvj94@gmail.com>
2017-10-26 18:00:02 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -21,11 +21,49 @@ requirejs(["utility/pace"], function(pace) {
 		}
 	      };
 	      reg.showNotification('Hello world!', options);
-	    });
+	      });
+	    }
 	  }
-	}
+		
+		if ('serviceWorker' in navigator) {
+		  navigator.serviceWorker.register('server-worker.js').then(function(reg) {
+		    console.log('Service Worker Registered!', reg);
+
+		    reg.pushManager.getSubscription().then(function(sub) {
+		      if (sub === null) {
+			// Update UI to ask user to register for Push
+			console.log('Not subscribed to push service!');
+		      } else {
+			// We have a subscription, update the database
+			console.log('Subscription object: ', sub);
+		      }
+		    });
+		  })
+		   .catch(function(err) {
+		    console.log('Service Worker registration failed: ', err);
+		  });
+		}
+	     
 	
-	
+		  function subscribeUser() {
+		  if ('serviceWorker' in navigator) {
+		    navigator.serviceWorker.ready.then(function(reg) {
+
+		      reg.pushManager.subscribe({
+			userVisibleOnly: true
+		      }).then(function(sub) {
+			console.log('Endpoint URL: ', sub.endpoint);
+		      }).catch(function(e) {
+			if (Notification.permission === 'denied') {
+			  console.warn('Permission for notifications was denied');
+			} else {
+			  console.error('Unable to subscribe to push', e);
+			}
+		      });
+		    })
+		  }
+		}
+
 	
 	
 		$(document).ready(function(){
