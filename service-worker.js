b0e2cc8cd5568f8fd7097f80a894fcc2cac95e4a
Svj94 <shreyasvj94@gmail.com>
2017-10-27 16:05:21 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -140,6 +140,25 @@ var filesToCache = [
 	'/Overlay/webcis/EUMainDashboardCISController.xml'
 ];
 
+
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('service-worker.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
