7873c24a6b298855f5ab9e9c58dbf7566f32ffaa
Svj94 <shreyasvj94@gmail.com>
2017-10-30 12:12:18 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker
-             .register('./service-worker.js')
+             .register('./Overlay/service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
 	  /* Access all files under app 
 	  .then(function(registration) {
